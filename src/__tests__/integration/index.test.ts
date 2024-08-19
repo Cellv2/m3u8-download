7d1b85ade63aff72d3485312ef7a24304b2db84a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-08-19 20:55:30 +0100

    Add in few more tests (lazy day)

--- src/__tests__/integration/index.test.ts
+++ src/__tests__/integration/index.test.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import { startServer, stopServer } from "../setup/express";
 
 beforeAll(() => {
@@ -16,7 +18,19 @@ describe("express server", () => {
     });
 
     it("serves the correct root page content", async () => {
+        const response = await fetch("http://localhost:3000");
+        const responseContent = await response.text();
 
-     })
+        const indexPath = path.join(
+            __dirname,
+            "../../../data/tests/index.html"
+        );
+        const indexContent = await fs.promises.readFile(indexPath, "utf-8");
 
+        expect(responseContent).toEqual(indexContent);
+    });
+
+    it("serves the correct m3u8 when mp4 path is hit", async () => {})
+
+    it("serves the correct mp4 when mp4 path is hit", async () => {})
 });
