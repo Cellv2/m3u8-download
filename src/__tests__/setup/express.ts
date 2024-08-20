a57a74de5f5c15c72ae71bdffb9a2c6630848a2c
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-08-20 21:04:24 +0100

    Add a way tp run express server at will

--- src/__tests__/setup/express.ts
+++ src/__tests__/setup/express.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import { Server } from "http";
 import path from "path";
-// const express = require("express");
-// const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../../../data/tests/hls-content")));
+app.use(
+    express.static(path.join(__dirname, "../../../data/tests/hls-content"))
+);
 
 // command to generate m3u8:
 // ffmpeg -i ../test.mp4  -c:a libmp3lame -ar 48000 -ab 64k  -c:v libx264 -b:v 128k -flags -global_header -map 0 -f segment -segment_list test.m3u8 -segment_time 2 -segment_format mpegts segment_%05d.ts
-const m3u8Path = path.join(__dirname, "../../../data/tests/hls-content/test.m3u8");
+const m3u8Path = path.join(
+    __dirname,
+    "../../../data/tests/hls-content/test.m3u8"
+);
 app.get("/m3u8", (req, res) => {
     res.sendFile(m3u8Path);
 });
@@ -25,19 +28,16 @@ app.get("/", (req, res) => {
     res.sendFile(indexPath);
 });
 
-// let server;
 let server: Server | null;
 
-// const startServer = () => {
 export const startServer = () => {
     const port = 3000;
     console.log(`Starting server on ${port}`);
     server = app.listen(port);
 };
 
-// startServer();
-
-// const stopServer = () => {
 export const stopServer = () => {
     server?.close();
 };
+
+startServer();
