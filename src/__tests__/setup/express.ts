5eb3a022f17f5ee3792f161a5d5f589022ee6890
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-08-24 19:42:04 +0100

    Clarify between file path and url paths

--- src/__tests__/setup/express.ts
+++ src/__tests__/setup/express.ts
@@ -10,22 +10,22 @@ app.use(
 
 // command to generate m3u8:
 // ffmpeg -i ../test.mp4  -c:a libmp3lame -ar 48000 -ab 64k  -c:v libx264 -b:v 128k -flags -global_header -map 0 -f segment -segment_list test.m3u8 -segment_time 2 -segment_format mpegts segment_%05d.ts
-export const m3u8Path = path.join(
+export const m3u8FilePath = path.join(
     __dirname,
     "../../../data/tests/hls-content/test.m3u8"
 );
 app.get("/m3u8", (req, res) => {
-    res.sendFile(m3u8Path);
+    res.sendFile(m3u8FilePath);
 });
 
-export const videoPath = path.join(__dirname, "../../../data/tests/test.mp4");
+export const videoFilePath = path.join(__dirname, "../../../data/tests/test.mp4");
 app.get("/video", (req, res) => {
-    res.sendFile(videoPath);
+    res.sendFile(videoFilePath);
 });
 
-export const indexPath = path.join(__dirname, "../../../data/tests/index.html");
+export const indexFilePath = path.join(__dirname, "../../../data/tests/index.html");
 app.get("/", (req, res) => {
-    res.sendFile(indexPath);
+    res.sendFile(indexFilePath);
 });
 
 let server: Server | null;
