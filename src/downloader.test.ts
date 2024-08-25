f305cd0e6a4f5cbb4ffc0641ed7340114bfb8f0b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-08-25 12:10:04 +0100

    Add some downloader tests

---
+++ src/downloader.test.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import {
+    m3u8FilePath,
+    startServer,
+    stopServer,
+} from "./__tests__/setup/express";
+import { downloader } from "./downloader";
+
+beforeAll(() => {
+    startServer();
+});
+
+afterAll(() => {
+    stopServer();
+});
+
+describe("downloader", () => {
+    it("should validate that the link is an m3u8 file", () => {});
+
+    it("should download the correct m3u8 at the link provided", async () => {
+        const downloaded = await downloader(`http://localhost:3000/m3u8`);
+
+        const fileAsBuffer = await fs.promises.readFile(m3u8FilePath);
+        expect(fileAsBuffer.equals(downloaded));
+    });
+
+    it("should throw if the link is invalid", async () => {
+        const url = "wrong";
+        const downloadedFunc = downloader(url);
+
+        await expect(downloadedFunc).rejects.toThrow(
+            TypeError(`Failed to parse URL from ${url}`)
+        );
+    });
+
+    it("should download all relevant mpegts files from the m3u8", () => {});
+});
---
+++ src/downloader.ts
@@ -0,0 +1,20 @@
+export const downloader = async (url: string): Promise<Buffer> => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(
+                `Received ${response.status} ${response.statusText} when trying to fetch ${url}`
+            );
+        }
+
+        const responseAsBuffer = Buffer.from(await response.arrayBuffer());
+
+        return responseAsBuffer;
+    } catch (error: unknown) {
+        if (!(error instanceof Error)) {
+            throw error;
+        }
+
+        throw new Error(error.message);
+    }
+};
