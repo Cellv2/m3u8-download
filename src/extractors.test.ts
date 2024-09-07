b8d9bd3adef27c0095d1723dd101ae1cca54950f
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-09-07 17:33:10 +0100

    Scaffold out some tests

---
+++ src/extractors.test.ts
@@ -0,0 +1,17 @@
+import * as extractors from "./extractors";
+
+describe(extractors.extractTsUrls, () => {
+    xit("should ...", () => {});
+});
+
+describe(extractors.extractM3u8Urls, () => {
+    xit("should ...", () => {});
+});
+
+describe(extractors.extractLines, () => {
+    xit("should ...", () => {});
+});
+
+describe(extractors.extractKeyUrls, () => {
+    xit("should ...", () => {});
+});
---
+++ src/extractors.ts
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/68574598/how-to-download-video-with-m3u8-extention-by-nodejs
+
+export const extractTsUrls = () => {};
+
+export const extractM3u8Urls = () => {};
+
+export const extractLines = () => {};
+
+export const extractKeyUrls = () => {};
