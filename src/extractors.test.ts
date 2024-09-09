fea3736776f051c16fe350bfb18e795caa560a99
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-09-09 23:13:25 +0100

    Red test

--- src/extractors.test.ts
+++ src/extractors.test.ts
@@ -1,7 +1,24 @@
 import * as extractors from "./extractors";
 
 describe(extractors.extractTsUrls, () => {
-    xit("should ...", () => {});
+    const m3u8FileContent = [
+        "#EXTM3U",
+        "#EXT-X-VERSION:3",
+        "#EXT-X-MEDIA-SEQUENCE:0",
+        "#EXT-X-ALLOW-CACHE:YES",
+        "#EXT-X-TARGETDURATION:9",
+        "#EXTINF:8.400000,",
+        "segment_00000.ts",
+        "#EXTINF:1.533333,",
+        "segment_00001.ts",
+        "#EXT-X-ENDLIST",
+    ].join("\n");
+
+    it("should return an array of all .ts relative URLs", () => {
+        const result = extractors.extractTsUrls(m3u8FileContent);
+
+        expect(result).toBe(["segment_00000.ts", "segment_00001.ts"]);
+    });
 });
 
 describe(extractors.extractM3u8Urls, () => {
--- src/extractors.ts
+++ src/extractors.ts
@@ -1,6 +1,8 @@
 // https://stackoverflow.com/questions/68574598/how-to-download-video-with-m3u8-extention-by-nodejs
 
-export const extractTsUrls = () => {};
+// https://datatracker.ietf.org/doc/html/rfc8216
+
+export const extractTsUrls = (fileContent: string) => {};
 
 export const extractM3u8Urls = () => {};
 
