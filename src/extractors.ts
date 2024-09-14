24fa74ebc43f082b343710e6ad976ad01e261066
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-09-14 11:25:25 +0100

    Well we can get rid of code now

--- src/extractors.ts
+++ src/extractors.ts
@@ -2,6 +2,11 @@
 
 // https://datatracker.ietf.org/doc/html/rfc8216
 
+// so it seems that ffmpeg accepts an URL to download...........
+// don't need to go through all of this!
+// https://stackoverflow.com/questions/47233304/how-to-download-m3u8-in-once-time
+// ffmpeg -i "https://your-url/videos/the-video.m3u8" -codec copy file.mp4
+
 export const extractTsUrls = (fileContent: string): string[] => {
     const lines = fileContent.split("\n");
     const tsUrls = lines.filter((line) => line.endsWith(".ts"));
