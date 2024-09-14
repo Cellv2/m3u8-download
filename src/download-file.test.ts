1adb58d02325b20bb7985f04b6d47dab398ba158
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-09-14 11:25:16 +0100

    Renaming

--- src/download-file.test.ts
+++
@@ -1,3 +0,0 @@
-describe("downloadFile", () => {
-    xit("should download all relevant mpegts files from the m3u8", () => {});
-});
---
+++ src/downloaders.test.ts
@@ -0,0 +1,9 @@
+describe("downloadFile", () => {
+    it("should return an array of mpegts files from the m3u8", () => {
+
+    });
+
+    it("should return an empty array if no mpegts files are found in the m3u8", () => {
+
+    })
+});
