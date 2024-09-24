97e72ec1c4e58ed6f86c68c7968762eeb82f3194
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-09-24 21:12:53 +0100

    Update coverage ignores

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+    coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/dist/"],
     testEnvironment: "node",
     transform: {
         "^.+.tsx?$": ["ts-jest", {}],
