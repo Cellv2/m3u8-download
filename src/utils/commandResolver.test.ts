269f5ef264d33e270ed43a233d6a58365740ffc8
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-10-29 20:47:30 +0000

    Fix tests

--- src/utils/commandResolver.test.ts
+++ src/utils/commandResolver.test.ts
@@ -1,17 +1,26 @@
 import commandExists from "command-exists-promise";
 import * as commandResolver from "./commandResolver";
 
-const mockedCommandExists = <jest.Mock<typeof commandExists>>(
-    (<unknown>jest.mocked("command-exists-promise"))
-);
+jest.mock("command-exists-promise");
+const mockedCommandExists = jest.mocked(commandExists);
+
+// const mockedCommandExists = <jest.Mock<typeof commandExists>>(
+//     (<unknown>jest.mocked("command-exists-promise"))
+// );
 
 describe(commandResolver.isCommandAvailable, () => {
     it("should return true if no args are passed and ffmpeg is available on the system", async () => {
-        mockedCommandExists.mockImplementationOnce(() => {
-            return function commandExists(command: any) {
-                return Promise.resolve(true);
-            };
-        });
+        // mockedCommandExists.mockImplementationOnce(() => {
+        //     return function commandExists(command: any) {
+        //         return Promise.resolve(true);
+        //     };
+        // });
+
+        // console.log(mockedCommandExists);
+        // console.log(commandExists)
+        mockedCommandExists.mockImplementationOnce(() => Promise.resolve(true));
+
+        // mockedCommandExists.mockResolvedValueOnce()
 
         const result = commandResolver.isCommandAvailable();
 
@@ -19,11 +28,15 @@ describe(commandResolver.isCommandAvailable, () => {
     });
 
     it("should return false if no args are passed and ffmpeg is not available on the system", async () => {
-        mockedCommandExists.mockImplementationOnce(() => {
-            return function commandExists(command: any) {
-                return Promise.resolve(false);
-            };
-        });
+        // mockedCommandExists.mockImplementationOnce(() => {
+        //     return function commandExists(command: any) {
+        //         return Promise.resolve(false);
+        //     };
+        // });
+
+        mockedCommandExists.mockImplementationOnce(() =>
+            Promise.resolve(false)
+        );
 
         const result = commandResolver.isCommandAvailable();
 
@@ -33,11 +46,13 @@ describe(commandResolver.isCommandAvailable, () => {
     it("should return true if command arg is passed and command is available on the system", async () => {
         const command = "iDoubtThisCommandExists";
 
-        mockedCommandExists.mockImplementationOnce(() => {
-            return function commandExists(command: any) {
-                return Promise.resolve(true);
-            };
-        });
+        // mockedCommandExists.mockImplementationOnce(() => {
+        //     return function commandExists(command: any) {
+        //         return Promise.resolve(true);
+        //     };
+        // });
+
+        mockedCommandExists.mockImplementationOnce(() => Promise.resolve(true));
 
         const result = commandResolver.isCommandAvailable(command);
 
@@ -47,11 +62,15 @@ describe(commandResolver.isCommandAvailable, () => {
     it("should return false if command arg is passed but command is not available on the system", async () => {
         const command = "iDoubtThisCommandExists";
 
-        mockedCommandExists.mockImplementationOnce(() => {
-            return function commandExists(command: any) {
-                return Promise.resolve(false);
-            };
-        });
+        // mockedCommandExists.mockImplementationOnce(() => {
+        //     return function commandExists(command: any) {
+        //         return Promise.resolve(false);
+        //     };
+        // });
+
+        mockedCommandExists.mockImplementationOnce(() =>
+            Promise.resolve(false)
+        );
 
         const result = commandResolver.isCommandAvailable(command);
 
@@ -59,11 +78,13 @@ describe(commandResolver.isCommandAvailable, () => {
     });
 
     it("should reject with an error if the command resolver errors", async () => {
-        mockedCommandExists.mockImplementationOnce(() => {
-            return function commandExists(command: any) {
-                return Promise.reject();
-            };
-        });
+        // mockedCommandExists.mockImplementationOnce(() => {
+        //     return function commandExists(command: any) {
+        //         return Promise.reject();
+        //     };
+        // });
+
+        mockedCommandExists.mockImplementationOnce(() => Promise.reject());
 
         const result = commandResolver.isCommandAvailable();
 
